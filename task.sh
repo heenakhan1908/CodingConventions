717e1ab6fa7c28c0543d924c0485c9f75e9a77ec
khan Heena <30309410+heenakhan1908@users.noreply.github.com>
2021-03-02 17:19:58 +0530

    Linux Task Submitted

--- task.sh
+++ task.sh
@@ -2,4 +2,4 @@
 read -p "Enter the location of the file" fileLocation
 
 ls -lsh $fileLocation 
-#this command will show the size of the fie in Human readable format i.e kb or mb
+#this command will show the size of the fie in Human Readable format i.e kb or mb
